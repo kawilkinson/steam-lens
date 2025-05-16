d4ecddd36f252077e12e3a0c7a9adf011fe0f700
Khazz0r <kaw3198@gmail.com>
2025-05-16 19:44:30 -0400

    add handlers for different steam endpoints

---
+++ internal/api/handler_api.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"net/http"
+	"strings"
+)
+
+type ApiConfig struct {
+	SteamApiKey string
+}
+
+func (apicfg *ApiConfig) HandlerGetPlayerSummaries(w http.ResponseWriter, req *http.Request) {
+	steamIDs := req.URL.Query().Get("steamids")
+	if steamIDs == "" {
+		RespondWithError(w, http.StatusBadRequest, "'steamids' parameter is required for getting player summaries", nil)
+		return
+	}
+
+	playerSummaries, err := apicfg.GetPlayerSummaries(strings.Split(steamIDs, ","))
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, "Unable to perform API call to Steam GetPlayerSummaries endpoint", err)
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, playerSummaries)
+}
+
+func (apicfg *ApiConfig) HandlerGetFriendList(w http.ResponseWriter, req *http.Request) {
+	steamID := req.URL.Query().Get("steamid")
+	if steamID == "" {
+		RespondWithError(w, http.StatusBadRequest, "'steamid' parameter is required for getting friend list", nil)
+		return
+	}
+
+	friends, err := apicfg.GetFriendList(steamID)
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, "Unable to perform API call to Steam GetFriendList endpoint", err)
+		return
+	}
+
+	friendIDs := []string{}
+	for _, friend := range friends.Friends {
+		friendIDs = append(friendIDs, friend.SteamID)
+	}
+
+	summaries, err := apicfg.GetPlayerSummaries(friendIDs)
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, "Unable to get player summaries of friends", err)
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, summaries)
+}
+
+func (apicfg *ApiConfig) HandlerGetOwnedGames(w http.ResponseWriter, req *http.Request) {
+	steamID := req.URL.Query().Get("steamid")
+	if steamID == "" {
+		RespondWithError(w, http.StatusBadRequest, "'steamid' parameter is required for getting player owned games", nil)
+		return
+	}
+
+	ownedGames, err := apicfg.GetOwnedGames(steamID)
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, "Unable to perform API call to Steam GetOwnedGames endpoint", err)
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, ownedGames)
+}
+
+func (apicfg *ApiConfig) HandlerGetPlayerAchievements(w http.ResponseWriter, req *http.Request) {
+	steamID := req.URL.Query().Get("steamid")
+	if steamID == "" {
+		RespondWithError(w, http.StatusBadRequest, "'steamid' parameter is required for getting player achievements", nil)
+		return
+	}
+
+	appID := req.URL.Query().Get("appid")
+	if appID == "" {
+		RespondWithError(w, http.StatusBadRequest, "'appid' parameter is required for getting player achievements", nil)
+		return
+	}
+
+	achievements, err := apicfg.GetPlayerAchievements(steamID, appID)
+	if err != nil {
+		RespondWithError(w, http.StatusBadRequest, "Unable to perform API call to Steam GetPlayerAchievements endpoint", err)
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, achievements)
+}
