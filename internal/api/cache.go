b966b932c5715f52019b45d9a7fcd706cc57d895
Khazz0r <kaw3198@gmail.com>
2025-05-23 00:01:05 -0400

    add cache for backend to reduce api calls and improvement performance

--- internal/api/cache.go
+++ internal/api/cache.go
@@ -1,1 +1,91 @@
 package api
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type CachedData[T any] struct {
+	Data       T
+	CachedTime time.Time
+}
+
+type Cache[T any] struct {
+	Cache     map[string]CachedData[T]
+	mu        sync.RWMutex
+	RenewTime time.Duration
+}
+
+func NewCache[T any](renewTime time.Duration) *Cache[T] {
+	return &Cache[T]{
+		Cache:     make(map[string]CachedData[T]),
+		RenewTime: renewTime,
+	}
+}
+
+func (cache *Cache[T]) ReadCache(steamID string) (T, bool) {
+	cache.mu.RLock()
+	defer cache.mu.RUnlock()
+
+	item, found := cache.Cache[steamID]
+	if !found {
+		var zero T
+		return zero, false
+	}
+	if time.Since(item.CachedTime) > cache.RenewTime {
+		var zero T
+		return zero, false
+	}
+
+	return cache.Cache[steamID].Data, true
+}
+
+func (cache *Cache[T]) CreateCacheData(steamID string, data T) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.Cache[steamID] = CachedData[T]{
+		Data:       data,
+		CachedTime: time.Now(),
+	}
+}
+
+type Cleaner[T any] struct {
+	Name     string
+	ticker   *time.Ticker
+	done     chan bool
+	Cache    *Cache[T]
+	Interval time.Duration
+}
+
+func (cleaner *Cleaner[T]) CacheCleanerStart() {
+	cleaner.ticker = time.NewTicker(cleaner.Interval)
+
+	go func() {
+		for {
+			select {
+			case <-cleaner.done:
+				return
+			case <-cleaner.ticker.C:
+				log.Printf("Cleaner being ran for cache '%s'\n", cleaner.Name)
+				cleaner.Cache.CleanCache()
+				log.Printf("Successfully removed expired entries from cache '%s'\n", cleaner.Name)
+			}
+		}
+	}()
+}
+
+func (cache *Cache[T]) CleanCache() {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	now := time.Now()
+
+	for steamID, item := range cache.Cache {
+		if now.Sub(item.CachedTime) > cache.RenewTime {
+			log.Printf("Clearing expired cache entry for steamID: %s\n", steamID)
+			delete(cache.Cache, steamID)
+		}
+	}
+}
