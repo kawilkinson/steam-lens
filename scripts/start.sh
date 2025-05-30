631f32888fa9cae5d1623a8eb469568d4df7ac55
Khazz0r <kaw3198@gmail.com>
2025-05-29 23:47:23 -0400

    add half-working dockerfile and docker compose file along with script

---
+++ scripts/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+until pg_isready -h "$DB_HOST" -U "$DB_USER"; do
+  >&2 echo "Postgres is unavailable - waiting..."
+  sleep 2
+done
+
+echo "Running goose migrations..."
+goose -dir /app/migrations "$DATABASE_URL" up
+
+echo "Starting server..."
+exec /app/server
