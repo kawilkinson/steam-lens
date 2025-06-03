087d718bff52a38e6a9f5cf3a568b8adacef7008
Khazz0r <kaw3198@gmail.com>
2025-06-03 12:53:50 -0400

    clean up env variables, comment out cd

--- frontend/src/app/definitions/urls.ts
+++ frontend/src/app/definitions/urls.ts
@@ -1,6 +1,6 @@
 export const apiURL = process.env.NODE_ENV === "production"
 	? process.env.NEXT_PUBLIC_API_URL
-	: "http://localhost:8080/api/steam/";
+	: "http://backend:8080/api/steam/";
 
 export const backendURL = process.env.NODE_ENV === "production"
 	? process.env.NEXT_PUBLIC_BACKEND_URL
